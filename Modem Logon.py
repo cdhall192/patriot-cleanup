edffe718f8dbc7689d8beca3253e1d46de3ad743
cdhall192 <chris.hall@arvig.com>
2017-05-24 17:55:44 -0500

    patriot-cleanup-modem-logon

---
+++ Modem Logon.py
@@ -0,0 +1,13 @@
+#def changeModem(modem_IP):
+
+    #TODO: Using IP from matchedList, go to modem and login
+    #TODO: Check if correct interface has the IP
+    #TODO: Go to network settings and disable unnecessary interfaces
+    #TODO: Logout
+    #TODO: Write errors in log
+
+
+
+#Iterates through list and runs changeModem on each IP
+#for IP in range(0, len(matched_list)):
+#    changeModem(matched_list[IP])
